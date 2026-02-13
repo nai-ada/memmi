651ee11026b8941b5542925fce4992d900302d9a
Nadia <naiya.ada003@gmail.com>
2026-02-13 01:06:14 -0800

    feature: install and initialize socket.io connection

--- client/src/components/InfoBar.vue
+++ client/src/components/InfoBar.vue
@@ -5,10 +5,35 @@ const userStore = useUserStore();
 </script>
 
 <template>
-  <div>
-    <img :src="userStore.userAvatar" />
-    <span>{{ userStore.userName }}</span>
+  <div class="wrapper">
+    <div class="user-wrapper">
+      <img :src="userStore.userAvatar" />
+      <span>{{ userStore.userName }}</span>
+    </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.wrapper {
+  padding: 6px;
+  border: 1px solid black;
+}
+
+.user-wrapper {
+  display: flex;
+  align-items: center;
+  width: fit-content;
+  padding: 5px 16px;
+  border: 1px solid black;
+  border-radius: 50px;
+}
+
+img {
+  width: 28px;
+  margin-right: 5px;
+}
+
+span {
+  font-size: 16px;
+}
+</style>
--- server/index.js
+++ server/index.js
@@ -1,21 +1,34 @@
-const express = require('express')
+const express = require("express");
 const { createServer } = require("http");
 const { Server } = require("socket.io");
-const cors = require('cors')
+const cors = require("cors");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 const httpServer = createServer(app);
-const io = new Server(httpServer);
 
-app.use(cors())
-
-io.on("connection", (socket) => {
-  console.log('Socket.io connection established')
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-app.get('/connect', (req, res) => {
-  res.json('Endpoint established')
-})
+app.use(cors());
 
-httpServer.listen({port}, () => console.log(`Server is running at port: ${port}`));
+io.on("connection", (socket) => {
+  socket.emit("Socket.io", "connected");
+  console.log("Socket.io connection established");
+
+  socket.on("message", (data) => {
+    console.log("Recieved from client:", data.message);
+  });
+});
+
+app.get("/connect", (req, res) => {
+  res.json("Application endpoint established");
+});
+
+httpServer.listen({ port }, () =>
+  console.log(`Server is running at port: ${port}`),
+);
