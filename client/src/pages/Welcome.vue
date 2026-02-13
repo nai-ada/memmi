743aec39b6169300cf047e0895b5745891fd9569
Nadia <naiya.ada003@gmail.com>
2026-02-12 19:26:45 -0800

    feature: add avatar selector

--- client/src/pages/Welcome.vue
+++ client/src/pages/Welcome.vue
@@ -1,16 +1,30 @@
-<!-- Composition API method (recommended) -->
 <script setup>
-import { ref } from 'vue'
-import { useRouter } from 'vue-router'
+import { ref } from "vue";
+import { useRouter } from "vue-router";
+import { onMounted } from "vue";
 
-const router = useRouter()
-const userName = ref("")
-const userAvatar = ref("")
+const router = useRouter();
+const userName = ref("");
+const userAvatar = ref("");
+const avatarOptions = import.meta.glob("@/assets/images/avatars/*.png", {
+  eager: true,
+  import: "default",
+});
+const avatars = ref(Object.values(avatarOptions));
 
-const goToWorkspace = () => {
-  console.log(userName.value, userAvatar.value)
-  router.push("/workspace")
+function getRandomAvatar() {
+  const randomIndex = Math.floor(Math.random() * avatars.value.length);
+  userAvatar.value = avatars.value[randomIndex];
 }
+
+const goToWorkspacePage = () => {
+  console.log(userName.value, userAvatar.value);
+  router.push("/workspace");
+};
+
+onMounted(() => {
+  getRandomAvatar();
+});
 </script>
 
 <template>
@@ -22,85 +36,20 @@ const goToWorkspace = () => {
       </div>
     </header>
     <main>
-      <div class="wrapper">
-        <div class="avatar-wrapper">
-          <h3>Avatar</h3>
-          <label for="avatar" hidden>Avatar</label>
-          <!-- avatar selector logic goes here -->
-          <div class="placeholder"></div>
-        </div>
-
-        <div class="name-wrapper">
-          <h3>Enter Name</h3>
-          <label for="name" hidden>Enter Name</label>
-          <input type="text" v-model="userName" id="name" name="name">
-        </div>
+      <h2>Choose an avatar and a nickname</h2>
+      <div class="avatar-selector">
+        <img :src="userAvatar" alt="Selected avatar" />
       </div>
-
-      <button type="submit" @click="goToWorkspace">Enter</button>
-
+      <button class="randomize" @click="getRandomAvatar">↻</button>
+      <label for="name" hidden>Enter Name</label>
+      <input type="text" v-model="userName" id="name" name="name" />
+      <button type="submit" @click="goToWorkspacePage">Enter</button>
     </main>
   </div>
 </template>
 
-<!-- using Options API method -->
-<!-- <script>
-export default {
-  name: "Welcome",
-   data() {
-    return {
-      userName: "",
-      userAvatar: "",
-    };
-  },
-  methods: {
-    goToWorkspace() {
-    console.log(this.userName, this.userAvatar);  
-      this.$router.push("/workspace");
-    }
-  }
-};
-</script> -->
-
 <style scoped>
-h1,
-h2 {
-  font-size: 2.5rem;
-  display: flex;
-  justify-content: center;
-  padding: 0;
-  margin: 0;
-  color: rgb(36, 36, 36);
-}
-
-h1 {
-  margin-top: 4rem;
-}
-
-h2,
-h3 {
-  font-weight: normal;
-}
-
-.wrapper {
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: center;
-  gap: 100px;
-  margin-top: 5rem;
-  padding: 2rem;
-  margin: 0 auto;
-}
-
-.avatar-wrapper {
-  display: flex;
-}
-
-.placeholder {
-  border: 1px solid rgb(133, 165, 255);
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  background: rgb(183, 199, 255);
+main {
+  background-color: rgb(255, 108, 108);
 }
 </style>
